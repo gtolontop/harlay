b1760f62fa70d79be53169858786e91a604a4acb
Cruck Store <cruckstore+github@gmail.com>
2026-03-15 22:47:42 +0100

    feat: Premium redesign of home page with parallax, animations and French navbar
    - Cinematic hero with parallax background and accent glow
    - Scroll-triggered reveal animations on all sections
    - Animated counters for specs (300km/h, 2000 units, 99%)
    - Feature cards with hover effects and accent line
    - Marquee ticker section for brand keywords
    - Story section with image hover zoom
    - Stock bar CTA and footer
    - Navbar translated to French (Accueil, Produits, À propos, Acheter)

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,12 +6,12 @@ class AppNavbar extends HTMLElement {
                     <a href="index.html" class="navbar-logo">HARLEY</a>
                     <div class="navbar-right">
                         <ul class="navbar-links">
-                            <li><a href="index.html">Home</a></li>
-                            <li><a href="products.html">Products</a></li>
-                            <li><a href="about.html">About Us</a></li>
+                            <li><a href="index.html">Accueil</a></li>
+                            <li><a href="products.html">Produits</a></li>
+                            <li><a href="about.html">À propos</a></li>
                         </ul>
-                        <a href="#" class="navbar-btn">
-                            <span>Buy Now</span>
+                        <a href="products.html" class="navbar-btn">
+                            <span>Acheter</span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
                                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
